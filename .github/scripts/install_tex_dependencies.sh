75e3ef2e3a68649745f80884c23c746ea42e9637
Pavel Sobolev <paveloom@mail.ru>
2020-05-14 18:02:52 +0000

    Тест с установкой matplotlib (5).

--- .github/scripts/install_tex_dependencies.sh
+++ .github/scripts/install_tex_dependencies.sh
@@ -3,18 +3,30 @@
 # Вывод названия скрипта
 printf "\nЗапущен скрипт для установки зависимостей.\n\n"
 
+# Установка pip3
+printf "Выполняется установка pip3...\n\n"
+sudo apt-get install python3-pip
+
+# Обновление pip3
+printf "Выполняется обновление pip3...\n\n"
+sudo python3 -m pip install --upgrade pip
+
+# Установка setuptools
+printf "Выполняется установка модуля setuptools...\n\n"
+pip3 install setuptools
+
 # Установка модуля matplotlib
-printf "Выполняется установка модуля matplotlib.\n\n"
+printf "Выполняется установка модуля matplotlib...\n\n"
 pip3 install matplotlib
 
 # Установка дополнительных шрифтов из пакета texlive
-printf "\nВыполняется установка дополнительных шрифтов из пакета texlive.\n\n"
+printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
 sudo apt-get install texlive-fonts-extra
 
 # Установка кириллических шрифтов из пакета texlive
-printf "\nВыполняется установка кириллических шрифтов из пакета texlive.\n\n"
+printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
 sudo apt-get install texlive-lang-cyrillic
 
 # Установка пакета cm-super
-printf "\nВыполняется установка пакета cm-super.\n\n"
+printf "\nВыполняется установка пакета cm-super...\n\n"
 sudo apt-get install cm-super
