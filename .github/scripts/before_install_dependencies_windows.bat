3f3dc7b3ec4ce811b9babc6a39b5f3d20f56fde2
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 17:46:42 +0000

    Тест с latex на Windows (3).

--- .github/scripts/before_install_dependencies_windows.bat
+++ .github/scripts/before_install_dependencies_windows.bat
@@ -2,10 +2,14 @@
 setlocal enableextensions
 
 :: Вывод названия скрипта
-echo. && echo Запущен скрипт для установки зависимостей на OSX. && echo.
+echo. && echo Запущен скрипт для установки зависимостей на Windows. && echo.
+
+:: Установка Python3
+echo Выполняется установка Python3... && echo.
+choco install python3
 
 :: Получение пути к Python
-echo Выполняется получение и сохранение пути к Python... && echo.
+echo. && echo Выполняется получение и сохранение пути к Python... && echo.
 
 for /F "tokens=* USEBACKQ" %%F in (`where python3`) do (set _PYTHON_PATH=%%F)
 set "_PYTHON_PATH=%_PYTHON_PATH:\=/%"
