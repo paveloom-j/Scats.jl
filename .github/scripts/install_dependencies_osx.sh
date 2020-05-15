fa5bbcd4d92150c0188c7256f5efe5bd683bf87c
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 07:25:13 +0000

    Тест с vis_test на OSX (2).

--- .github/scripts/install_dependencies_osx.sh
+++ .github/scripts/install_dependencies_osx.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Вывод названия скрипта
-printf "\nЗапущен скрипт для установки зависимостей.\n\n"
+printf "\nЗапущен скрипт для установки зависимостей на OSX.\n\n"
 
 # Обновление списков пакетов
 printf "Выполняется обновление списка пакетов...\n\n"
-sudo apt-get update
+sudo brew update
 
 # Установка dvipng
 printf "\nВыполняется установка dvipng...\n\n"
-sudo apt-get install dvipng
+sudo brew install dvipng
 
 # Установка python3-dev
 printf "\nВыполняется установка python3...\n\n"
-sudo apt-get install python3-dev
+sudo brew install python3-dev
 
 # Установка pip3
 printf "\nВыполняется установка pip3...\n\n"
-sudo apt-get install python3-pip
+sudo brew install python3-pip
 
 # Обновление pip3
 printf "\nВыполняется обновление pip3...\n\n"
@@ -33,16 +33,16 @@ pip3 install matplotlib
 
 # Установка пакета texlive-latex-extra
 printf "\nВыполняется установка пакета texlive-latex-extra...\n\n"
-sudo apt-get install texlive-latex-extra
+sudo brew install texlive-latex-extra
 
 # Установка дополнительных шрифтов из пакета texlive
 printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
-sudo apt-get install texlive-fonts-extra
+sudo brew install texlive-fonts-extra
 
 # Установка кириллических шрифтов из пакета texlive
 printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
-sudo apt-get install texlive-lang-cyrillic
+sudo brew install texlive-lang-cyrillic
 
 # Установка пакета cm-super
 printf "\nВыполняется установка пакета cm-super...\n\n"
-sudo apt-get install cm-super
+sudo brew install cm-super
---
+++ .github/scripts/install_dependencies_unix.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Вывод названия скрипта
+printf "\nЗапущен скрипт для установки зависимостей на Unix системах.\n\n"
+
+# Обновление списков пакетов
+printf "Выполняется обновление списка пакетов...\n\n"
+sudo apt-get update
+
+# Установка dvipng
+printf "\nВыполняется установка dvipng...\n\n"
+sudo apt-get install dvipng
+
+# Установка python3-dev
+printf "\nВыполняется установка python3...\n\n"
+sudo apt-get install python3-dev
+
+# Установка pip3
+printf "\nВыполняется установка pip3...\n\n"
+sudo apt-get install python3-pip
+
+# Обновление pip3
+printf "\nВыполняется обновление pip3...\n\n"
+python3 -m pip install --upgrade pip
+
+# Установка setuptools
+printf "\nВыполняется установка модуля setuptools...\n\n"
+pip3 install setuptools
+
+# Установка модуля matplotlib
+printf "\nВыполняется установка модуля matplotlib...\n\n"
+pip3 install matplotlib
+
+# Установка пакета texlive-latex-extra
+printf "\nВыполняется установка пакета texlive-latex-extra...\n\n"
+sudo apt-get install texlive-latex-extra
+
+# Установка дополнительных шрифтов из пакета texlive
+printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
+sudo apt-get install texlive-fonts-extra
+
+# Установка кириллических шрифтов из пакета texlive
+printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
+sudo apt-get install texlive-lang-cyrillic
+
+# Установка пакета cm-super
+printf "\nВыполняется установка пакета cm-super...\n\n"
+sudo apt-get install cm-super
