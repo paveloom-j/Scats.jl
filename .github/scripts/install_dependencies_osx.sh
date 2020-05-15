a10a029ecd72be03c47aa94582bc8a5e4aea0c2e
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 08:16:40 +0000

    Тест с vis_test на OSX (6).

--- .github/scripts/install_dependencies_osx.sh
+++ .github/scripts/install_dependencies_osx.sh
@@ -3,21 +3,21 @@
 # Вывод названия скрипта
 printf "\nЗапущен скрипт для установки зависимостей на OSX.\n\n"
 
-# Обновление списков пакетов
-printf "Выполняется обновление списка пакетов...\n\n"
-brew update
+# Получение скрипта macports-ci
+printf "Выполняется скачивание скрипта macports-ci...\n\n"
+curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
+
+# Установка MacPorts
+printf "\nВыполняется установка MacPorts...\n\n"
+source macports-ci install
+
+# Установка ccache
+printf "\nВыполняется установка ccache...\n\n"
+source macports-ci ccache
 
 # Установка dvipng
 printf "\nВыполняется установка dvipng...\n\n"
-sudo tlmgr update --self && sudo tlmgr install dvipng
-
-# Установка python3-dev
-printf "\nВыполняется установка python3...\n\n"
-brew install python3-dev
-
-# Установка pip3
-printf "\nВыполняется установка pip3...\n\n"
-brew install python3-pip
+port install dvipng
 
 # Обновление pip3
 printf "\nВыполняется обновление pip3...\n\n"
@@ -33,16 +33,20 @@ pip3 install matplotlib
 
 # Установка пакета texlive-latex-extra
 printf "\nВыполняется установка пакета texlive-latex-extra...\n\n"
-brew install texlive-latex-extra
+port install texlive-latex-extra
 
 # Установка дополнительных шрифтов из пакета texlive
 printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
-brew install texlive-fonts-extra
+port install texlive-fonts-extra
 
 # Установка кириллических шрифтов из пакета texlive
 printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
-brew install texlive-lang-cyrillic
+port install texlive-lang-cyrillic
 
 # Установка пакета cm-super
 printf "\nВыполняется установка пакета cm-super...\n\n"
-brew install cm-super
+port install cm-super
+
+# Сохранение пакетов в кеш
+printf "\nСохранение пакетов в кеш...\n\n"
+source macports-ci ccache --save
