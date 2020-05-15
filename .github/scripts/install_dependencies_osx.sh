c8372a2db32573e912b1746b2b4c293be0427741
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 13:48:51 +0000

    Тест с vis_test на OSX (20).

--- .github/scripts/install_dependencies_osx.sh
+++ .github/scripts/install_dependencies_osx.sh
@@ -3,38 +3,11 @@
 # Вывод названия скрипта
 printf "\nЗапущен скрипт для установки зависимостей на OSX.\n\n"
 
-# # Получение скрипта macports-ci
-# printf "Выполняется скачивание скрипта macports-ci...\n\n"
-# curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
-
-# # Установка MacPorts
-# printf "\nВыполняется установка MacPorts...\n\n"
-# source macports-ci install
-
-# # Установка ccache
-# printf "\nВыполняется установка ccache...\n\n"
-# source macports-ci ccache
-
-# # Установка dvipng
-# printf "\nВыполняется установка dvipng...\n\n"
-# sudo port -N install dvipng
-
-# # Обновление списка пакетов
-# printf "Выполняется обновление списка пакетов...\n\n"
-# brew update
-
-# # Установка пакета mactex
-# printf "\nВыполняется установка пакета mactex...\n\n"
-# brew cask install mactex
-
+# Скачивание скриптов
+printf "\nВыполняется скачивание скриптов для автоматической установки TexLive...\n\n"
 wget https://raw.githubusercontent.com/y-yu/install-tex-travis/master/install-tex.sh
 wget https://raw.githubusercontent.com/y-yu/install-tex-travis/master/tlmgr.sh
 chmod +x install-tex.sh tlmgr.sh
-
-# Обновление путей после установки basictex
-printf "\nВыполняется обновление путей после установки basictex...\n\n"
-# eval "$(/usr/libexec/path_helper)"
-latex -v
 
 # Обновление pip3
 printf "\nВыполняется обновление pip3...\n\n"
@@ -44,21 +17,17 @@ python3 -m pip install --upgrade pip
 printf "\nВыполняется установка модуля matplotlib...\n\n"
 pip3 install matplotlib
 
-# # Установка пакета texlive-latex-extra
-# printf "\nВыполняется установка пакета texlive-latex-extra...\n\n"
-# sudo port -N install texlive-latex-extra
+# Установка пакета texlive-latex-extra
+printf "\nВыполняется установка пакета collection-latexextra...\n\n"
+sudo tlmgr install collection-latexextra
 
 # # Установка дополнительных шрифтов из пакета texlive
-# printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
-# sudo port -N install texlive-fonts-recommended
+printf "\nВыполняется установка дополнительных шрифтов из пакета texlive...\n\n"
+sudo tlmgr install collection-fontsextra
 
-# # Установка кириллических шрифтов из пакета texlive
-# printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
-# sudo port -N install texlive-lang-cyrillic
-
-# # Сохранение пакетов в кеш
-# printf "\nСохранение пакетов в кеш...\n\n"
-# source macports-ci ccache --save
+# Установка кириллических шрифтов из пакета texlive
+printf "\nВыполняется установка кириллических шрифтов из пакета texlive...\n\n"
+sudo tlmgr install collection-langcyrillic
 
 # Получение и сохранение пути к Python
 printf "\nВыполняется получение и сохранение пути к Python...\n\n"
