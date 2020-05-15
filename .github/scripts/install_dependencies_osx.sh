3cabaa1405764f292f7d11707bba2b5d83f88a51
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 12:31:27 +0000

    Тест с vis_test на OSX (16).

--- .github/scripts/install_dependencies_osx.sh
+++ .github/scripts/install_dependencies_osx.sh
@@ -23,9 +23,9 @@ printf "\nЗапущен скрипт для установки зависимо
 printf "Выполняется обновление списка пакетов...\n\n"
 brew update
 
-# Установка пакета basictex
-printf "\nВыполняется установка пакета basictex...\n\n"
-brew cask install basictex
+# Установка пакета mactex
+printf "\nВыполняется установка пакета mactex...\n\n"
+brew cask install mactex
 
 # Обновление pip3
 printf "\nВыполняется обновление pip3...\n\n"
