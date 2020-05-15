2a3595dc8a6a1fb53bc00da6de8799c69f324745
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 18:03:19 +0000

    Тест с latex на Windows (6).

--- .github/scripts/before_install_dependencies_windows.bat
+++ .github/scripts/before_install_dependencies_windows.bat
@@ -6,4 +6,11 @@ echo. && echo Запущен скрипт для установки Python3 на
 :: Установка Python3
 echo Выполняется установка Python3... && echo.
 choco install python3
+
+:: Сохранение пути к Python в переменной окружения PYTHON
+echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON...
+
+md %USERPROFILE%\.julia\config
+echo ENV["PYTHON"]="C:/ProgramData\chocolatey/bin/python3" > %USERPROFILE%\.julia\config\startup.jl
+
 refreshenv
