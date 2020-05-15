aa83bf2745685cecc9b35c6ccadadbe5df91d2a2
Pavel Sobolev <paveloom@mail.ru>
2020-05-15 14:35:02 +0000

    Тест с vis_test на OSX (23).

--- .github/scripts/install_dependencies_osx.sh
+++ .github/scripts/install_dependencies_osx.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Вывод названия скрипта
+printf "\nЗапущен скрипт для установки TexLive и необходимых пакетов.\n\n"
+
+# Запуск скрипта для автоматической установки TexLive и tlmgr
+printf "Выполняется установка TexLive и tlmgr...\n\n"
+. ./install-tex.sh
+
+# Установка пакета dvipng
+printf "\nВыполняется установка пакета dvipng...\n\n"
+sudo tlmgr install dvipng
+
 # Установка пакета texlive-latex-extra
 printf "\nВыполняется установка пакета collection-latexextra...\n\n"
 sudo tlmgr install collection-latexextra
--- .github/scripts/install_dependencies_unix.sh
+++ .github/scripts/install_dependencies_unix.sh
@@ -3,46 +3,6 @@
 # Вывод названия скрипта
 printf "\nЗапущен скрипт для установки зависимостей на Unix системах.\n\n"
 
-# Скачивание пакета MacPorts 2.6 для macOS 10.15 Catalina
-
-printf "Выполняется скачивание пакета.\n\n"
-curl -JOL 'https://github.com/macports/macports-base/releases/download/v2.6.2/MacPorts-2.6.2-10.15-Catalina.pkg'
-
-# Получение полного пути к файлу
-
-printf "\nВыполняется получение полного пути к файлу.\n\n"
-
-## Источник скрипта: https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
-
-TARGET_FILE="MacPorts-2.6.2-10.15-Catalina.pkg"
-
-cd `dirname $TARGET_FILE`
-TARGET_FILE=`basename $TARGET_FILE`
-
-## Продолжать итерацию по (возможной) цепочке символьных ссылок
-
-while [ -L "$TARGET_FILE" ]
-do
-    TARGET_FILE=`readlink $TARGET_FILE`
-    cd `dirname $TARGET_FILE`
-    TARGET_FILE=`basename $TARGET_FILE`
-done
-
-## Получение абсолютного пути целевого файла с помощью нахождения физического
-## пути той директории, в которой мы находимся, и присоединения к нему
-## имени целевого файла.
-
-PHYS_DIR=`pwd -P`
-RESULT=$PHYS_DIR/$TARGET_FILE
-
-printf "Полный путь к целевому файлу:\n"
-printf $RESULT
-
-# Установка пакета
-
-printf "\n\nВыполняется установка пакета.\n\n"
-sudo installer -verbose -pkg $RESULT -target /
-
 # Обновление списков пакетов
 printf "Выполняется обновление списка пакетов...\n\n"
 sudo apt-get update
