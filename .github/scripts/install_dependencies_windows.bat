d97d2e6cfd27f84e1324c81e25f9fc3265c95142
Pavel Sobolev <paveloom@mail.ru>
2020-05-16 09:49:59 +0000

    Тест с latex на Windows (24).

--- .github/scripts/install_dependencies_windows.bat
+++ .github/scripts/install_dependencies_windows.bat
@@ -13,13 +13,10 @@ python -m pip install matplotlib
 
 :: Установка MiKTeX
 echo. && echo Выполняется установка MiKTeX... && echo.
-REM wget https://miktex.org/download/win/miktexsetup-x64.zip
-REM unzip *.zip
-REM miktexsetup --package-set=basic download
-REM miktexsetup install
-REM choco install miktex
-curl -o miktex.exe https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-2.9.7417-x64.exe
-miktex --unattended --auto-install=yes
+wget https://miktex.org/download/win/miktexsetup-x64.zip
+unzip *.zip
+miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=complete download
+miktexsetup --verbose --local-package-repository=C:\miktex-repository --shared --user-config="%APPDATA%\MiKTeX\2.9" --user-data="%LOCALAPPDATA%\MiKTeX\2.9" --user-install=<APPDATA>\MiKTeX\2.9" --print-info-only install
 
 :: Сохранение пути к Python в переменной окружения PYTHON
 echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON... && echo.
