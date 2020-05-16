e0d4f91bbb4de4161df65cc3a0dda378b18cd335
Pavel Sobolev <paveloom@mail.ru>
2020-05-16 10:53:01 +0000

    Тест с latex на Windows (26).

--- .github/scripts/install_dependencies_windows.bat
+++ .github/scripts/install_dependencies_windows.bat
@@ -17,6 +17,11 @@ wget https://miktex.org/download/win/miktexsetup-x64.zip
 unzip *.zip
 miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=complete download
 miktexsetup --verbose --local-package-repository=C:\miktex-repository --shared --user-config="%APPDATA%\MiKTeX\2.9" --user-data="%LOCALAPPDATA%\MiKTeX\2.9" --user-install="%APPDATA%\MiKTeX\2.9" --print-info-only install
+dir C:\Program Files\MiKTeX 2.9
+dir C:\Program Files\MiKTeX 2.9\miktex
+dir C:\Program Files\MiKTeX 2.9\miktex\bin
+export PATH="/c/Program\\ Files/MiKTeX\\ 2.9/miktex/bin:$PATH"
+
 
 :: Сохранение пути к Python в переменной окружения PYTHON
 echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON... && echo.
