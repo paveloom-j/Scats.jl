6bdff5d75bde5884e452aa9fb9f48a3545e79948
Pavel Sobolev <paveloom@mail.ru>
2020-05-16 07:35:48 +0000

    Тест с latex на Windows (17).

--- .github/scripts/before_install_dependencies_windows.bat
+++ .github/scripts/before_install_dependencies_windows.bat
@@ -4,22 +4,5 @@
 echo. && echo Запущен скрипт для установки Python3 на Windows. && echo.
 
 :: Установка Python3
-echo Выполняется установка Python3... && echo.
-choco install python3
-
-:: Установка matplotlib
-echo. && echo Выполняется установка matplotlib... && echo.
-C:/Python38/python.exe -m pip install matplotlib
-
-:: Установка texlive
-echo. && echo Выполняется установка TexLive... && echo.
-.github/scripts/tl64.bat
-
-:: Сохранение пути к Python в переменной окружения PYTHON
-echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON... && echo.
-
-C:/Python38/python.exe -V
-echo.
-
-md %USERPROFILE%\.julia\config
-echo ENV["PYTHON"]="C:/Python38/python.exe" > %USERPROFILE%\.julia\config\startup.jl
+:: echo Выполняется установка Python3... && echo.
+:: choco install python3
---
+++ .github/scripts/install_dependencies_windows.bat
@@ -0,0 +1,25 @@
+@echo off
+
+:: Вывод названия скрипта
+echo. && echo Запущен скрипт для установки зависимостей на Windows. && echo.
+
+:: Обновление pip
+echo Выполняется обновление pip... && echo.
+python -m pip install --upgrade pip
+
+:: Установка matplotlib
+echo. && echo Выполняется установка matplotlib... && echo.
+python -m pip install matplotlib
+
+:: Установка texlive
+echo. && echo Выполняется установка TexLive... && echo.
+wget http://mirrors.mi.ras.ru/CTAN/systems/texlive/tlnet/install-tl.zip
+
+:: Сохранение пути к Python в переменной окружения PYTHON
+echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON... && echo.
+
+C:/Python38/python.exe -V
+echo.
+
+md %USERPROFILE%\.julia\config
+echo ENV["PYTHON"]="C:/Python38/python.exe" > %USERPROFILE%\.julia\config\startup.jl
