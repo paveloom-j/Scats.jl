202cc990ecc8861679bb59f98daa9e44a59fcd88
Pavel Sobolev <paveloom@mail.ru>
2020-05-16 22:39:09 +0000

    Тест с latex на Windows (43).

--- .github/scripts/install_dependencies_windows.bat
+++ .github/scripts/install_dependencies_windows.bat
@@ -15,15 +15,8 @@ python -m pip install matplotlib
 echo. && echo Выполняется установка MiKTeX... && echo.
 wget https://miktex.org/download/win/miktexsetup-x64.zip
 unzip *.zip
-miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=basic download
-miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=basic --shared --user-config="%APPDATA%\MiKTeX\2.9" --user-data="%LOCALAPPDATA%\MiKTeX\2.9" --user-install="%APPDATA%\MiKTeX\2.9" install
-set PATH="C:\Program Files\MiKTeX 2.9\miktex\bin\x64;%PATH%"
-dir /b /s initexmf.exe
-REM C:\Program Files\MiKTeX 2.9\miktex\bin\x64\initexmf --update-fndb
-REM C:\Program Files\MiKTeX 2.9\miktex\bin\x64\initexmf --mkmaps
-REM C:\Program Files\MiKTeX 2.9\miktex\bin\x64\initexmf --mklinks --force
-REM C:\Program Files\MiKTeX 2.9\miktex\bin\x64\mpm --find-updates
-REM C:\Program Files\MiKTeX 2.9\miktex\bin\x64\mpm --update
+miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=essential download
+miktexsetup --verbose --local-package-repository=C:\miktex-repository --package-set=essential --shared --user-config="%APPDATA%\MiKTeX\2.9" --user-data="%LOCALAPPDATA%\MiKTeX\2.9" --user-install="%APPDATA%\MiKTeX\2.9" install
 
 :: Сохранение пути к Python в переменной окружения PYTHON
 echo. && echo Выполняется сохранение пути к Python в переменную окружения PYTHON... && echo.
