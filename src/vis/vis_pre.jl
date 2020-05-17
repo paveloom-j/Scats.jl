d94de17bd0c2aeeb0e33449f43eb4886d26ad0ab
Pavel Sobolev <paveloom@mail.ru>
2020-05-17 09:38:28 +0000

    Тест с latex на Windows (49).

--- src/vis/vis_pre.jl
+++ src/vis/vis_pre.jl
@@ -15,10 +15,7 @@ rcP["figure.dpi"] = 300
 rcP["text.usetex"] = true
 
 # Включение поддержки русского языка
-rcP["text.latex.preamble"] = [raw"\usepackage[main=russian,english]{babel}", raw"\usepackage{cmlgc}"]
-
-# Установка семейства шрифтов для текста внутри математической моды
-rcP["mathtext.fontset"] = "cm"
+rcP["text.latex.preamble"] = [raw"\usepackage[main=russian,english]{babel}"]
 
 # Установка размеров шрифта
 rcP["font.size"] = 18
