f3946b7694371f9f3d2c3858c0d588ffdfc17381
Pavel Sobolev <paveloom@mail.ru>
2020-06-10 20:14:05 +0000

    Исправлена документация для Scats.internal.prec.RF.

--- src/prec.jl
+++ src/prec.jl
@@ -9,18 +9,20 @@ Module containing types of numbers used in the package, and also their according
 module prec
 export IT, RT, RF
 
-# Определение типа целых чисел
 "Type of integer values. Default value: `typeof(1)`."
 IT = typeof(1)
 
-# Определение типа вещественных чисел
 "Type of real values. Default value: `typeof(1.0)`."
 RT = typeof(1.0)
 
-"Format of real values. Default value: `typeof(1.0)`."
+"""
+Format of real values. Default values:
+- for [`RT`](@ref) = `Float64`: `"% .15E"`;
+- for [`RT`](@ref) = `Float32`: `"% .6E"`;
+- for [`RT`](@ref) = `Float16`: `"% .3E"`.
+"""
 RF = "% .6E"
 
-# Определение формата для вывода вещественных чисел
 if (RT == Float64)
     RF = "% .15E"
 elseif (RT == Float16)
